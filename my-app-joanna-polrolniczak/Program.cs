919d5f71cd62a38ba17d30f2ce0e5311d3a7ced3
Joanna Półrolniczak <joanna.polrolniczak@relativity.com>
2022-11-02 16:29:40 +0100

    swap

--- my-app-joanna-polrolniczak/Program.cs
+++ my-app-joanna-polrolniczak/Program.cs
@@ -3,6 +3,6 @@ var app = builder.Build();
 
 var configuration = (IConfiguration) app.Services.GetService(typeof(IConfiguration))!;
 
-app.MapGet("/", () => $"Hello World! xd Value: {configuration.GetSection("MyKey").Value}");
+app.MapGet("/", () => $"Hello World! xd Value: {configuration.GetSection("MyKey").Value} swapped from staging");
 
 app.Run();
